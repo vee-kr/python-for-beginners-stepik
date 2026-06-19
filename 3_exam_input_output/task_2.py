41db586c8108425197b048878df6a2b5f1fcd9b3
veegit config --global user.email korchagina.vm2009@gmail.comgit config --global user.name vee <korchagina.vm2009@gmail.com>
2026-06-19 22:59:05 +0300

    completed 2 module mini exam (input-output, variables, sep/end operators)

---
+++ 3_exam_input_output/task_2.py
@@ -0,0 +1,3 @@
+num_1, num_2 = int(input()), int(input())
+print('Квадрат суммы', num_1, 'и', num_2, 'равен', (num_1 + num_2) ** 2)
+print('Сумма квадратов', num_1, 'и', num_2, 'равна', (num_1 ** 2) + (num_2 ** 2))
---
+++ 3_exam_input_output/task_4.py
@@ -0,0 +1,5 @@
+num = int(input())
+dig_1 = num
+dig_2 = num * 10  + num
+dig_3 = num * 100 + num * 10 + num
+print(dig_1+dig_2+dig_3)
