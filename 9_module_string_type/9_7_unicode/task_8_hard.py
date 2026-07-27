4d1ab9a4e3fe4b18b908e31ec49c101d06de91ad
veegit config --global user.email korchagina.vm2009@gmail.comgit config --global user.name vee <korchagina.vm2009@gmail.com>
2026-07-27 10:29:36 +0300

    added another solution for task

--- 9_module_string_type/9_7_unicode/task_8_hard.py
+++ 9_module_string_type/9_7_unicode/task_8_hard.py
@@ -1,7 +1,7 @@
 num, cipher = int(input()), input()
 for char in cipher:
 
-    cur_code = ord(char)
+    cur_code = ord(char)  # 1
     new_code = cur_code - num
 
     if (new_code >= ord('a')) and (new_code <= ord('z')):
@@ -10,3 +10,11 @@ for char in cipher:
     else:
         new_letter = chr((ord('z') - (num - (cur_code - ord('a')))) + 1)
         print(new_letter, end='')
+
+    # ---------------------------------------------
+
+    if new_code < ord('a'):  # 2
+        new_letter = new_code + 26
+        print(chr(new_letter), end='')
+    else:
+        print(chr(new_code), end='')
