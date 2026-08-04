faeca201e94ebda0fd0b02cf022a8ede4ee18033
veegit config --global user.email korchagina.vm2009@gmail.comgit config --global user.name vee <korchagina.vm2009@gmail.com>
2026-08-04 11:01:58 +0300

    completed theme 11.7 about list comprehensions

---
+++ 11_module_lists/11_7_list_comprehensions/task_1.py
@@ -0,0 +1,7 @@
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+            'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+            'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+new_keywords = [keyword[1:] for keyword in keywords]
+
+print(new_keywords)
---
+++ 11_module_lists/11_7_list_comprehensions/task_7.py
@@ -0,0 +1,2 @@
+cubes = [int(i) ** 3 for i in input().split()]
+print(*cubes)
