caa0e099de7b2f0dab118b2092fa421f72ec853b
veegit config --global user.email korchagina.vm2009@gmail.comgit config --global user.name vee <korchagina.vm2009@gmail.com>
2026-08-07 21:14:28 +0300

    completed theme 13.1 about functions with no parameters

---
+++ 13_functions/13_1_functions_with_no_parameters/task_1.py
@@ -0,0 +1,8 @@
+def draw_box():
+    print('*' * 10)
+    for _ in range(12):
+        print('*', '*', sep=' ' * 8)
+    print('*' * 10)
+
+
+draw_box()
---
+++ 13_functions/13_1_functions_with_no_parameters/task_2.py
@@ -0,0 +1,6 @@
+def draw_triangle():
+    for i in range(1, 11):
+        print('*' * i)
+
+
+draw_triangle()
